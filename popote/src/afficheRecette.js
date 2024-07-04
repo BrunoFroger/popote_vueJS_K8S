65b1c1d31abad3f8e0ffff2be3cc654f198a29de
Bruno FROGER <bruno.froger@orange.com>
2024-07-04 23:39:09 +0200

    update affichage ingredients

--- popote/src/afficheRecette.js
+++ popote/src/afficheRecette.js
@@ -137,7 +137,7 @@ export default {
         console.log('afficheRecette.js => loadDatas : recette (' + this.recette.titre + ') recupérée')
         console.log('afficheRecette.js => loadDatas : recette (' + this.$parent.recette.titre + ') recupérée')
         this.ingredients = {}
-        recetteJson.foreach((item, index) => {
+        for (item in recetteJson){
           console.log("afficheRecette.js => loadDatas : extraction ingredient : " + item.ingredient)
           var ingredient = {
             nom: item.ingredient,
