c5fe98389d1bf1e1e4f5a800a8383322cff74303
Bruno FROGER <bruno.froger@orange.com>
2024-07-10 14:42:05 +0200

    ajout pages admin gestion users et recettes

---
+++ popote/src/adminGereRecettes.js
@@ -0,0 +1,41 @@
+
+
+export default {
+    props: [], 
+      data: function () {
+        return {
+          currentDateTime: '',
+        };
+      },
+      mounted() {
+        this.updateDateTime();
+        this.updateDatas();
+        setInterval(this.updateDateTime, 1000);
+      },
+      template: '\
+        <div>\
+          <h1>Administration : Gestion des recettes</h1>\
+          <p>Cette page est accessible uniquement pour les administrateurs du site</p>\
+        </div> \
+      ',
+      methods: {
+        //---------------------------------
+        //
+        //  UpdateTime
+        //
+        //---------------------------------
+        updateDateTime() {
+          const now = new Date();
+          this.currentDateTime = now.toLocaleString();
+        },
+        //---------------------------------
+        //
+        //  updateDatas
+        //
+        //---------------------------------
+        updateDatas() {
+          console.log("adminGereRecettes.js => updateDatas")
+        },
+      }
+}
+
