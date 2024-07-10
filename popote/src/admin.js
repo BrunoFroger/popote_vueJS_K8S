f12e04eb782ec387c109b8490404e55f8e7079ff
Bruno FROGER <bruno.froger@orange.com>
2024-07-10 14:21:41 +0200

    ajout pages admin gestion users et recettes

--- popote/src/admin.js
+++ popote/src/admin.js
@@ -37,12 +37,12 @@ export default {
                 <tr> \
                     <td>nombre de users</td> \
                     <td>{{nbUsers}}</td> \
-                    <td @onClick = "changeModeAffichage(\'gereUsers\')">Gestion des Users</td> \
+                    <!--td @onClick = "changeModeAffichage(\'gereUsers\')">Gestion des Users</td--> \
                 </tr> \
                 <tr> \
                     <td>nombre de recettes</td> \
                     <td>{{nbRecettes}}</td> \
-                    <td@onClick = "changeModeAffichage(\'gereRecettes\')">Gestion des recettes</td> \
+                    <!--td@onClick = "changeModeAffichage(\'gereRecettes\')">Gestion des recettes</td--> \
                 </tr> \
             </tbody> \
           </table> \
