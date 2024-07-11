7fa2395c304f0ebc9c191f571184191a25338e0d
Bruno FROGER <bruno.froger@orange.com>
2024-07-11 10:44:22 +0200

    update admin gestion recettes

--- popote/src/adminGereRecettes.js
+++ popote/src/adminGereRecettes.js
@@ -57,6 +57,7 @@ export default {
         //---------------------------------
         updateDatas() {
           console.log("adminGereRecettes.js => updateDatas")
+          this.getAllRecettes()
         },
         //---------------------------------
         //
