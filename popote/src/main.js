f9d33d9e06e6380e4a49d47719dec4f0047afaaa
Bruno FROGER <bruno.froger@orange.com>
2024-07-16 14:09:59 +0200

    supprime test variable globale frontend

--- popote/src/main.js
+++ popote/src/main.js
@@ -22,8 +22,8 @@ const app = createApp({
           currentDateTime: '',
           //serverNodeAdress: 'http://localhost:3000',
           //serverNodeAdress: 'http://popote.zapto.org:3000',
-          //serverNodeAdress: 'https://popote.zapto.org/api',
-          serverNodeAdress: process.env.BACKEND_IP,
+          serverNodeAdress: 'https://popote.zapto.org/api',
+          //serverNodeAdress: process.env.BACKEND_IP,
           
         }
     },
