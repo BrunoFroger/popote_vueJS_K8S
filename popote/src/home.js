28dab6e1f382b3a764f18d64e9deac0e3d0c8246
Bruno FROGER <bruno.froger@orange.com>
2024-07-19 11:47:11 +0200

    creation page creation recette

--- popote/src/home.js
+++ popote/src/home.js
@@ -20,7 +20,7 @@ export default {
           <li>Recettes : accès a l\'ensemble des recettes disponibles</li>\
           <li>Mon compte : gestion de votre conpte (connexion / deconnexion / données personnelles</li>\
           </ul>\
-          <p><font color="red">Site experimental en cours de construction (support de formation) aucune donnée n\'est valide ni conservée</font></p>\
+          <p stype="color:#FF0000">Site experimental en cours de construction (support de formation) aucune donnée n\'est valide ni conservée</p>\
         </div>\
       ',
       methods: {
