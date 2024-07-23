570b3b59d635d0705f4635568b7e5be6776a99e2
Bruno FROGER <bruno.froger@orange.com>
2024-07-23 15:25:03 +0200

    Update recettesListe.js

--- popote/src/recettesListe.js
+++ popote/src/recettesListe.js
@@ -66,8 +66,8 @@ export default {
             <td @click="loadRecette(item.id)">{{item.auteur}}</td>\
             <td @click="loadRecette(item.id)">{{item.type}}</td>\
             <td @click="loadRecette(item.id)">{{item.description}}</td>\
-            <td v-if="userConnected">Validée</td>\
-            <td v-else>En attente de validation</td>\
+            <td v-if="userConnected && item.validation != 0">Validée</td>\
+            <td v-if="userConnected && item.validation == 0">En attente de validation</td>\
           </tr>\
         </table>\
         <button @click="pagePrecedente">recettes précédentes</button>\
