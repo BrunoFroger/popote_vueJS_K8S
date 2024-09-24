334ac9ed0478c5353fdce0f6e9f2de3a5506d486
Bruno FROGER <bruno.froger@orange.com>
2024-09-24 15:32:40 +0200

    reprise mode monopod avec adresse des containeurs en localhost

--- serverNode/src/docker_cmd.bash
+++ serverNode/src/docker_cmd.bash
@@ -11,9 +11,9 @@ echo "==========================="
 #echo root123 | su -c "/etc/init.d/ssh start" 
 #node src/server
 #sleep 1000
-echo "Lancement du server node desactivé temporairement"
-echo "connectez vous sur le pod et taper la commande : "
-echo "<node src/server_avec_mariadb> pour le relancer"
+# echo "Lancement du server node desactivé temporairement"
+# echo "connectez vous sur le pod et taper la commande : "
+# echo "<node src/server_avec_mariadb> pour le relancer"
 # while :
 # do
 #     sleep 10
--- serverNode/src/server_avec_mariadb.js
+++ serverNode/src/server_avec_mariadb.js
@@ -340,7 +340,7 @@ const server = http.createServer((req, res) => {
 server.listen(port, () => {
     var db = mysql.createConnection({
         // host: process.env.IP_MARIADB,
-        host: localhost,
+        host: "localhost",
         database: "Popote",
         user: "popote",   
         password: "popote123" 
